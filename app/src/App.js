7b2c8d3943cb3194c2c0a21911827edd15dbfa3a
Jeremy <jeremy@opsone.net>
2017-03-14 22:58:16 +0100

    Add dropzone for pictures

--- app/src/App.js
+++ app/src/App.js
@@ -3,13 +3,18 @@ import React, { Component } from 'react';
 import TopBar from './containers/TopBar/Bar';
 import Aside from './containers/Aside';
 
+const style = {
+  paddingTop: 64,
+  height: "calc(100% - 64px)"
+};
+
 class App extends Component {
   render() {
     return (
       <div>
         <TopBar />
 
-        <div style={{paddingTop: 64, height: "100%"}}>
+        <div style={style}>
           { this.props.children }
         </div>
 
